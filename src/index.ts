465a4beb7398a4959482010cba2c38bf3aa42e42
Ismail Hosen <engineerismail21@gmail.com>
2026-08-17 13:27:18 +0600

    6320-gummas-global-stock

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ http.createServer(app).listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
 
   const SYNC_INTERVAL = 2 * 60 * 60 * 1000;
-  const FORCE_TIMES_PER_DAY = 24;
+  const FORCE_TIMES_PER_DAY = 6;
   let lastForceSlot = '';
 
   scheduleNextSync();
